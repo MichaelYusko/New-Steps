bb15380a441ef879f8e838089e8b81dc91bda311
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-09-27 23:02:35 +0300

    Encode categorical data

--- Data_Preprocessing/data_preprocessing_template.py
+++ Data_Preprocessing/data_preprocessing_template.py
@@ -3,7 +3,7 @@
 import numpy as np
 import matplotlib as plt
 import pandas as pd
-from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder
 
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
@@ -16,3 +16,13 @@ imputer = imputer.fit(X[:, 1:3])
 
 # Transform missing data
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding categorical data
+label_encoder = LabelEncoder()
+X[:, 0] = label_encoder.fit_transform(X[:, 0])
+
+one_hot_encoder = OneHotEncoder(categorical_features = [0])
+X = one_hot_encoder.fit_transform(X).toarray()
+
+label_encoder_Y = LabelEncoder()
+Y = label_encoder_Y.fit_transform(Y)
