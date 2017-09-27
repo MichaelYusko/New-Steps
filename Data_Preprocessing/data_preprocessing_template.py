6cb993b3926e23a28a287efee847e58880baaa91
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-09-27 21:29:54 +0300

    Transform missing data

--- Data_Preprocessing/data_preprocessing_template.py
+++ Data_Preprocessing/data_preprocessing_template.py
@@ -3,7 +3,16 @@
 import numpy as np
 import matplotlib as plt
 import pandas as pd
+from sklearn.preprocessing import Imputer
 
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 3].values
+
+# strategy/axis parameters by default is 'mean' and 0
+# But for example, I added both.
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+imputer = imputer.fit(X[:, 1:3])
+
+# Transform missing data
+X[:, 1:3] = imputer.transform(X[:, 1:3])
